bca0fafccad2716b26d971a996083112b162e062
Arman <arman.tapayev@gmail.com>
2022-01-12 22:46:08 +0600

    conditional

---
+++ Chapter_2/lesson_6/script.js
@@ -0,0 +1,17 @@
+const num = "50";
+num == 50 ? console.log("OK!") : console.log("Error");
+
+switch (num) {
+  case "45":
+    console.log("forty five");
+    break;
+  case "63":
+    console.log("sixty three");
+    break;
+  case "50":
+    console.log("fifty");
+    break;
+  default:
+    console.log("default");
+    break;
+}
