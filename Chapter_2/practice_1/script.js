a0e6cfe02e683ea04841112a1120ecef9c1c36c2
Arman <arman.tapayev@gmail.com>
2022-01-12 23:16:47 +0600

    practice 1 next task

--- Chapter_2/practice_1/script.js
+++ Chapter_2/practice_1/script.js
@@ -8,12 +8,35 @@ const personalMovieDB = {
   private: false,
 };
 
-const a = prompt("Last seen movie: ", ""),
-  b = prompt("Your assessment: ", ""),
-  c = prompt("Last seen movie: ", ""),
-  d = prompt("Your assessment: ", "");
+// const a = prompt("Last seen movie: ", ""),
+//   b = prompt("Your assessment: ", ""),
+//   c = prompt("Last seen movie: ", ""),
+//   d = prompt("Your assessment: ", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Last seen movie: ", ""),
+    b = prompt("Your assessment: ", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Too less movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("You are classical viewer");
+} else if (personalMovieDB.count >= 30) {
+  console.log("You are moviefill");
+} else {
+  console.log("Error");
+}
 
 console.log(personalMovieDB);
